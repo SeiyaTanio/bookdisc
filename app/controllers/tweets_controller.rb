2075726c0afc87a02bd1f9205b85b470a1177fe3
SeiyaTanio <basket.cp3.spc@icloud.com>
2021-12-14 07:13:11 +0900

    new、createアクションの定義、ルーティング

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,4 +3,13 @@ class TweetsController < ApplicationController
   def index
     @tweets = Tweet.includes(:user)
   end
+
+  def new
+    @tweet = Tweet.new
+  end
+
+  def create
+    @tweet = Tweet.new
+    @tweet.save
+  end
 end
