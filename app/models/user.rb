eda04b15c404c752bd67e9d30b3866eb116b7835
SeiyaTanio <basket.cp3.spc@icloud.com>
2021-12-13 18:34:53 +0900

    エラーメッセージの日本語化

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,5 @@ class User < ApplicationRecord
   validates :nickname, presence: true
   
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX, message: 'is invalid. Include both letters and numbers'
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'が無効です。英字と数字の両方が必要です。'
 end
