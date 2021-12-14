233698f9242ceb9b1f7ef169d5ef77b40ec3877d
SeiyaTanio <basket.cp3.spc@icloud.com>
2021-12-14 15:15:06 +0900

    index,new,createアクションの定義

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,20 @@
 class ProfilesController < ApplicationController
 
+  def index
+  end
+
+  def new
+    @profile = Profile.new
+  end
+
+  def create
+    @profile = Profile.new(profile_params)
+    if @profile.save
+      redirect_to root_path
+    else
+      render :new
+    end
+
   private
     def profile_params
       params.require(:profile).permit(:favorite_book, :self_introduction).merge(user_id: current_user.id)
