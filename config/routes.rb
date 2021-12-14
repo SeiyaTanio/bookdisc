84bd57e0876b7310b551f4f70d773563f69db699
SeiyaTanio <basket.cp3.spc@icloud.com>
2021-12-14 15:49:20 +0900

    ルーティングの再設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "tweets#index"
-  resources :tweets do
-    resources :profiles, only: [:index, :new, :create]
-  end
+  resources :tweets
+  resources :profiles, only: [:index, :new, :create]
 end
