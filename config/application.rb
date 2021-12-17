39230106ecd643048b6d0904f639f8e0efb2095f
SeiyaTanio <basket.cp3.spc@icloud.com>
2021-12-17 12:44:30 +0900

    アセットパイプラインの整理

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ module Bookdisc
     # 日本語の言語設定
     config.i18n.default_locale = :ja
     config.time_zone = 'Tokyo'
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
