f95c3b446c42e3e8a6eabc3d70c9f426e237a798
SeiyaTanio <basket.cp3.spc@icloud.com>
2021-12-17 18:39:05 +0900

    blogsテーブルの生成

--- db/migrate/20211217093343_create_blogs.rb
+++ db/migrate/20211217093343_create_blogs.rb
@@ -1,7 +1,9 @@
 class CreateBlogs < ActiveRecord::Migration[6.0]
   def change
     create_table :blogs do |t|
-
+      t.string :title, null: false
+      t.text :article, null: false
+      t.references :user, null: false, foreign_key: true
       t.timestamps
     end
   end
