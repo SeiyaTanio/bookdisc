8d7ee7b235e68b0dee544f8e7af062f6fe85fe6a
SeiyaTanio <basket.cp3.spc@icloud.com>
2021-12-18 09:49:48 +0900

    ブログ新規投稿ページ作成

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -4,8 +4,20 @@ class BlogsController < ApplicationController
   end
 
   def new
+    @blog = Blog.new
   end
 
   def create
+    @blog = Blog.new(blog_params)
+    if @blog.save
+      redirect_to blogs_path
+    else
+      render :new
+    end
   end
+
+  private
+  def blog_params
+    params.require(:blog).permit(:title, :article).merge(user_id: current_user.id)
+  end 
 end
