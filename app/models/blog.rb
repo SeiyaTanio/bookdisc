2658a1d38b962a0e5d340c1f76a59e4e2ffd3e9e
SeiyaTanio <basket.cp3.spc@icloud.com>
2021-12-18 12:34:39 +0900

    文字数制限のバリデーションの追加、入力フォームの修正

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,6 +2,6 @@ class Blog < ApplicationRecord
 
   belongs_to :user
 
-  validates :title, presence: true
+  validates :title, presence: true, length: { maximum: 50 }
   validates :article, presence: true
 end
