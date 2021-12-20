a908715f6344953f24498853d85608376cdba8de
SeiyaTanio <basket.cp3.spc@icloud.com>
2021-12-20 11:58:16 +0900

    ルーム作成、更新時のユーザー未選択時のバリデーション設定

--- app/models/room.rb
+++ app/models/room.rb
@@ -3,4 +3,5 @@ class Room < ApplicationRecord
   has_many :users, through: :user_rooms, validate: false
 
   validates :room_name, presence: true, length: { maximum: 30 }
+  validates :user_ids, length: { minimum: 2 }
 end
