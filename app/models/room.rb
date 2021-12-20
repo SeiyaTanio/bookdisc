33d7c349cf090b26e087b5fbe6842a92c0be2f80
SeiyaTanio <basket.cp3.spc@icloud.com>
2021-12-20 10:29:18 +0900

    ルーム名30文字以下のバリデーション設定

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,5 +2,5 @@ class Room < ApplicationRecord
   has_many :user_rooms, dependent: :destroy
   has_many :users, through: :user_rooms, validate: false
 
-  validates :room_name, presence: true
+  validates :room_name, presence: true, length: { maximum: 30 }
 end
