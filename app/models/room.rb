ba538c1c9926abeab5e5ac5cb26d74cbf2a1402a
SeiyaTanio <basket.cp3.spc@icloud.com>
2021-12-21 08:35:45 +0900

    messagesテーブルの作成

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,6 +1,7 @@
 class Room < ApplicationRecord
   has_many :user_rooms, dependent: :destroy
   has_many :users, through: :user_rooms, validate: false
+  has_many :messages
 
   validates :room_name, presence: true, length: { maximum: 30 }
   validates :user_ids, length: { minimum: 2 }
