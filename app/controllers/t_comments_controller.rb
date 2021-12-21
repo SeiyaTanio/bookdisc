7afd41e989be0f60a94b103239f5052be07e8feb
SeiyaTanio <basket.cp3.spc@icloud.com>
2021-12-21 15:38:38 +0900

    createアクションの定義、ルーティング設定

--- app/controllers/t_comments_controller.rb
+++ app/controllers/t_comments_controller.rb
@@ -1,2 +1,13 @@
 class TCommentsController < ApplicationController
+
+  def create
+    t_comment = TComment.create(t_comment_params)
+    redirect_to tweet_path(t_comment.tweet_id)
+  end
+
+  private
+  def t_comment_params
+    params.require(:t_comment).permit(:t_text).merge(user_id: current_user.id, tweet_id: params[:tweet_id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "tweets#index"
-  resources :tweets
+  resources :tweets do
+    resources :t_comments, only: [:create]
+  end
   resources :profiles, only: [:index, :new, :create, :edit, :update]
   resources :blogs
   resources :rooms do
