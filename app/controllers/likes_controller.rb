09b979a3d925b45f81c62eb5530b8ce6d0e1563c
SeiyaTanio <basket.cp3.spc@icloud.com>
2021-12-22 17:59:38 +0900

    いいね解除機能の実装

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,13 +1,15 @@
 class LikesController < ApplicationController
 
   def create
-    if current_user.like_this(clicked_tweet)
-      flash[:success] = '投稿にいいねしました'
-      redirect_back(fallback_location: root_path)
-    else
-      flash[:alart] = 'いいねに失敗しました'
-      redirect_back(fallback_location: root_path)
-    end
+    current_user.like_this(clicked_tweet)
+    flash[:success] = '投稿にいいねしました'
+    redirect_back(fallback_location: root_path)
+  end
+
+  def destroy
+    current_user.likes.find_by(tweet_id: params[:tweet_id]).destroy
+    flash[:danger] = '「いいね！」を解除しました。'
+    redirect_back(fallback_location: root_path)
   end
 
   private
