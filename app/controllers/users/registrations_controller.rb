6f856080f62ea478469275efb3bb453d86422f74
SeiyaTanio <basket.cp3.spc@icloud.com>
2022-01-04 17:58:17 +0900

    アカウント作成後、プロフィール作成画面に遷移

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -56,9 +56,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+  def after_sign_up_path_for(resource)
+    new_profile_path(profile_sort: 0)
+  end
 
   # The path used after sign up for inactive accounts.
   # def after_inactive_sign_up_path_for(resource)
