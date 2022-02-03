54e58d03029d4246e3713c7d4978cd8dfbeb756b
SeiyaTanio <basket.cp3.spc@icloud.com>
2022-02-03 16:28:33 +0900

    likeモデルの単体テストコード

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    user = FactoryBot.create(:user)
+    tweet = FactoryBot.create(:tweet)
+    @like = FactoryBot.build(:like, user_id: user.id, tweet_id: tweet.id)
+  end
+
+  describe 'ツイートのいいね機能' do
+    context 'いいねできる場合' do
+      it '全ての情報が正しく存在すれば投稿できる' do
+        expect(@like).to be_valid
+      end
+    end
+    context 'いいねできない場合' do
+      it 'ユーザーidが空では送信できない' do
+        @like.user = nil
+        @like.valid?
+        expect(@like.errors.full_messages).to include('User must exist')
+      end
+      it 'ツイートidが空では送信できない' do
+        @like.tweet = nil
+        @like.valid?
+        expect(@like.errors.full_messages).to include('Tweet must exist')
+      end
+    end
+  end
 end
